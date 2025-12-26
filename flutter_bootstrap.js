a2e2e5f513d61eb09982f729132c27aa6b023535
rocauto <zhaopengioo@live.cn>
2025-12-26 20:21:50 +0800

    dev countdown_clock

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "438651325"
+    serviceWorkerVersion: "3173731711"
   }
 });
